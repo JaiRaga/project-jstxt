ca88d7142b528a6ff81e33bcd5332bbc01f62708
JaiRaga <raga.j.santhosh@gmail.com>
2020-11-06 12:00:43 +0530

    tested code compilation

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
 require("./db/mongoose");
+const { node } = require("compile-run");
 
 const app = express();
 
 //init middleware
 app.use(express.json());
+
+// -----------------------------------------------------------------------------
+// Compiler code
+const srccode = `function name() {
+    console.log("Hello Raga")
+}
+name()`;
+const resultloc = node.runSource(srccode);
+
+resultloc.then((res) => console.log(res)).catch((err) => console.log(err));
+
+const result = node.runFile(`./app.js`, { stdin: "3\n2 " });
+
+result.then((res) => console.log(res.stdout)).catch((err) => console.log(err));
+
+// -----------------------------------------------------------------------------
 
 const port = process.env.PORT || 9008;
 
