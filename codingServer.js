27aae036a025f6a62fdb0396d5e9e0e789d1da22
JaiRaga <raga.j.santhosh@gmail.com>
2020-11-08 11:50:11 +0530

    server to serve compiled code

---
+++ codingServer.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const serverport = 8000;
+
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+    console.log(path.join(__dirname, "public", req.url));
+    if (req.url === "/") {
+      fs.readFile("./public/index.html", (err, html) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(html);
+      });
+    } else if (req.url.match(".css")) {
+      let fStream = fs.createReadStream("./public/styles.css", "UTF-8");
+      res.writeHead(200, { "Content-Type": "text/css" });
+      fStream.pipe(res);
+    } else if (req.url.match(".js")) {
+      console.log(req.url.match(".js"));
+      let fStream = fs.createReadStream("./public/script.js", "UTF-8");
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      fStream.pipe(res);
+    }
+  })
+  .listen(serverport);
