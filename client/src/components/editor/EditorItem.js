affa4232763bb385ba4739c616498ce45e861550
JaiRaga <raga.j.santhosh@gmail.com>
2020-11-17 15:21:37 +0530

    added editor title component

--- client/src/components/editor/EditorItem.js
+++ client/src/components/editor/EditorItem.js
@@ -1,15 +1,27 @@
 import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core";
 import "../home/Home.css";
 import Editor from "../editor/Editor";
+import EditorTitle from "./EditorTitle";
 import CodeSnapShot from "../editor/CodeSnapShot";
 import LineNumber from "../line/LineNumber";
 
-const EditorItem = ({ editorId }) => {
+// const useStyles = makeStyles((theme) => ({
+//   title: {
+//     backgroundColor: "blue",
+//     zIndex: 5,
+//     gridColumn: "4 / -1",
+//     gridRow: "1"
+//   }
+// }));
+
+const EditorItem = ({ editorid }) => {
   return (
     <Fragment>
       <div className='editor'>
         {/* <LineNumber /> */}
-        <Editor editorId={editorId} />
+        <EditorTitle title={editorid} />
+        <Editor editorid={editorid} />
 
         {/* <CodeSnapShot /> */}
       </div>
